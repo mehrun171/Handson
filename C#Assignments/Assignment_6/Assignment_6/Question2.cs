7a7b1792bdeccce42558dd37cf5c03d6bcdea6cf
Shamshi Mehrun <shamshim@infinite.com>
2025-07-07 10:32:40 +0530

    Assignment_6 done

---
+++ C#Assignments/Assignment_6/Assignment_6/Question2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+    /*    2. Write a program in C# Sharp to create a file and write an array of strings to the file.
+    */
+    class Question2
+    {
+        static void Main()
+        {
+            List<string> stringlines = new List<string>();
+            Console.Write("Enter number of strings: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter string {0}", i + 1);
+                string s = Console.ReadLine();
+                stringlines.Add(s);
+            }
+            FileStream fs = new FileStream("C:\\mehr\\Infinite_Hands_on\\C#Assignments\\Assignment_6\\Assignment_6\\Question_2_Text.txt",
+                FileMode.Append, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(fs);
+
+            foreach (var str in stringlines)
+            {
+                writer.WriteLine(str);
+            }
+            writer.Close();
+
+            Console.WriteLine("File created and data written successfully.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#Assignments/Assignment_6/Assignment_6/Question3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_6
+{
+    class Question3
+    {
+        static void Main()
+        {
+            try
+            {
+                FileStream fs = new FileStream("C:\\mehr\\Infinite_Hands_on\\C#Assignments\\Assignment_6\\Assignment_6\\SampleFile.txt",
+                    FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                int linecount = 0;
+                string str = sr.ReadLine();
+                while (str != null)
+                {
+                    linecount++;
+                    Console.WriteLine(str);
+                    str = sr.ReadLine();
+                }
+                Console.WriteLine("Number of lines in file: {0}", linecount);
+                sr.Close();
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception Occurred!!!" + e.Message);
+            }
+            Console.Read();
+        }
+    }
+}
