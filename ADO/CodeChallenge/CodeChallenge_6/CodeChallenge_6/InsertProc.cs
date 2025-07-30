8aafaba5c1de856a77789508377c247603a773a1
Shamshi Mehrun <shamshim@infinite.com>
2025-07-30 11:39:44 +0530

    CodeChallenge 6 done

---
+++ ADO/CodeChallenge/CodeChallenge_6/CodeChallenge_6/InsertProc.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CodeChallenge_6
+{
+    class DataAccess
+    {
+        static SqlConnection con = null;
+        static SqlCommand cmd = null;
+
+        public SqlConnection getConnection()
+        {
+            string connect = "Data Source=ICS-LT-4FFZC64\\SQLEXPRESS;Initial Catalog=CodeChallengeDB;" +
+                             "User ID=sa;Password=Mehrunshamshi@77;";
+            con = new SqlConnection(connect);
+            con.Open();
+            return con;
+        }
+
+        public int InsertEmployeeData(string name, string gender, double salary)
+        {
+            int empId = 0;
+            try
+            {
+                con = getConnection();
+                cmd = new SqlCommand("InsertEmployeeDetails", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+                cmd.Parameters.AddWithValue("@Salary", salary);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    empId = reader.GetInt32(0);
+                }
+                reader.Close(); 
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return empId;
+        }
+
+        
+    }
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+
+        DataAccess da = new DataAccess();
+
+        public int InsertEmployee()
+        {
+            Console.WriteLine("Enter Name of the Employee:");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter Gender of the Employee:");
+            Gender = Console.ReadLine();
+            Console.WriteLine("Enter Salary of the Employee:");
+            Salary = Convert.ToDouble(Console.ReadLine());
+
+            return da.InsertEmployeeData(Name, Gender, Salary);
+        }
+    }
+
+    class InsertProc
+    {
+            public static void Main()
+            {
+                Employee emp = new Employee();
+                int empId = emp.InsertEmployee();
+                if (empId > 0)
+                {
+                    Console.WriteLine($"Employee added successfully with EmpId: {empId}");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to add employee.");
+                }
+            Console.ReadLine();
+            }
+        }
+}
---
+++ ADO/CodeChallenge/CodeChallenge_6/CodeChallenge_6/UpdateProc.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CodeChallenge_6
+{
+    class DataAccesss
+    {
+        static SqlConnection con = null;
+        static SqlCommand cmd = null;
+
+        public SqlConnection getConnection()
+        {
+            string connect = "Data Source=ICS-LT-4FFZC64\\SQLEXPRESS;Initial Catalog=CodeChallengeDB;" +
+                             "User ID=sa;Password=Mehrunshamshi@77;";
+            con = new SqlConnection(connect);
+            return con;
+        }
+
+        public double UpdateSalary(int empId)
+        {
+            double updatedSalary = 0;
+
+            try
+            {
+                con = getConnection();
+                cmd = new SqlCommand("UpdateEmployeeSalary", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpId", empId);
+                cmd.Parameters.Add("@UpdatedSalary", SqlDbType.Decimal).Direction = ParameterDirection.Output;
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+                updatedSalary = Convert.ToDouble(cmd.Parameters["@UpdatedSalary"].Value);
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("SQL Error: " + e.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return updatedSalary;
+        }
+
+        public void DisplayEmployeeDetails(int empId)
+        {
+            try
+            {
+                con = getConnection();
+                cmd = new SqlCommand("SELECT EmpId, Name, Gender, Salary FROM Employee_Details WHERE EmpId = @EmpId", con);
+                cmd.Parameters.AddWithValue("@EmpId", empId);
+
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    Console.WriteLine("Employee Details After Update:");
+                    Console.WriteLine($"EmpId: {reader["EmpId"]}");
+                    Console.WriteLine($"Name: {reader["Name"]}");
+                    Console.WriteLine($"Gender: {reader["Gender"]}");
+                    Console.WriteLine($"Salary: {reader["Salary"]}");
+                }
+                else
+                {
+                    Console.WriteLine("No employee found with the given EmpId.");
+                }
+
+                reader.Close();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("SQL Error: " + e.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+    }
+
+    class UpdateProc
+    {
+        static void Main()
+        {
+            DataAccesss da = new DataAccesss();
+
+            Console.WriteLine("Enter EmpId to update salary:");
+            int empId = Convert.ToInt32(Console.ReadLine());
+
+            double newSalary = da.UpdateSalary(empId);
+            if (newSalary > 0)
+            {
+                Console.WriteLine($"Salary updated successfully. New Salary: {newSalary}");
+            }
+            else
+            {
+                Console.WriteLine("Failed to update salary or employee not found.");
+            }
+            da.DisplayEmployeeDetails(empId);
+
+            Console.ReadLine();
+        }
+    }
+}
